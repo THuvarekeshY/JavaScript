853783eacb4d74ac6d5d9ff3bfc4a53f39692f93
Thuvara <166202354+THuvarekeshY@users.noreply.github.com>
2024-10-04 14:25:31 +0530

    Add files via upload

---
+++ webcolour/colour.js
@@ -0,0 +1,18 @@
+function Red(){
+    document.getElementById("body").style.backgroundColor="Red"
+    document.getElementById("Red").style.backgroundColor="Red"
+}
+function Blue(){
+    document.getElementById("body").style.backgroundColor="blue"
+    document.getElementById("Blue").style.backgroundColor="blue"
+}
+function Green(){
+    document.getElementById("body").style.backgroundColor="green"
+    document.getElementById("green").style.backgroundColor="green"
+}
+function Reset(){
+    document.getElementById("body").style.backgroundColor="white"
+    document.getElementById("Red").style.backgroundColor="white"
+    document.getElementById("Blue").style.backgroundColor="white"
+    document.getElementById("green").style.backgroundColor="white"
+}
